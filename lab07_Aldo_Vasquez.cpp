0f07b0aabd1fa827012fcc7b59c7bcb32902d751
AldoVasquez07 <142806675+AldoVasquez07@users.noreply.github.com>
2023-11-03 22:01:59 -0500

    Creando Estructura principal del código
    Primero que nada, se ha creado la función Torneo, tal como se ha solicitado en el pdf del lab07. De esta manera, se cita en el main, la primera prueba, la cual será enviada a la función, la cual todavía permanece vacía.

---
+++ lab07_Aldo_Vasquez.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+vector<int> Torneo(vector<int> habilidades, int N, int K) {
+    int tam = habilidades.size();
+
+    queue<int> torneoQueue;
+
+    return habilidades;
+}
+
+int main() {
+    vector<int> habilidades = {1,2,3}
+    vector<int> caso1 = Torneo(habilidades, 2, 2);
+}
