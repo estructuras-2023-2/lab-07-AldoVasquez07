1120c9841adaeb34280c19cde1b9fc1b5a847df3
AldoVasquez07 <142806675+AldoVasquez07@users.noreply.github.com>
2023-11-03 22:22:53 -0500

    Estableciendo adecuadamente el return
    Se ha implementado el return de una manera correcta, de esta manera, se regresa un vector el cual contiene el perdedor y el ganador, a su vez, se han implementado los casos de prueba, los cuales han sido solicitados en el pdf.

--- lab07_Aldo_Vasquez.cpp
+++ lab07_Aldo_Vasquez.cpp
@@ -4,12 +4,11 @@
 
 using namespace std;
 
-
-vector<int> Torneo(vector<int> habilidades, int N, int K) {
+vector<int> simularTorneo(vector<int> habilidades, int N, int K) {
     int tam = habilidades.size();
 
     queue<int> torneoQueue;
-    
+
     for (int i = 2; i < tam; i++) {
         torneoQueue.push(habilidades[i]);
     }
@@ -19,35 +18,50 @@ vector<int> Torneo(vector<int> habilidades, int N, int K) {
     int jugador2 = habilidades[1];
 
     vector<int> resultado(2);
-    
     while (K > 0) {
-    if (jugador1 > jugador2) {
-        rondasGanadas++;
-        resultado = {jugador2, jugador1};
-        torneoQueue.push(jugador2);
-    } else {
-        rondasGanadas = 1;
-        resultado = {jugador1, jugador2};
-        torneoQueue.push(jugador1);
-        jugador1 = jugador2;
-    }
-    
-    if (rondasGanadas == N) {
-        torneoQueue.push(jugador1);
-        jugador1 = torneoQueue.front();
-        torneoQueue.pop();
-        rondasGanadas = 0;
-    }
-    jugador2 = torneoQueue.front();
-    torneoQueue.pop();
-    
-    K--;
-}
+        if (jugador1 > jugador2) {
+            rondasGanadas++;
+            resultado = {jugador2, jugador1};
+            torneoQueue.push(jugador2);
+        } else {
+            rondasGanadas = 1;
+            resultado = {jugador1, jugador2};
+            torneoQueue.push(jugador1);
+            jugador1 = jugador2;
+        }
 
-    return habilidades;
+        if (rondasGanadas == N) {
+            torneoQueue.push(jugador1);
+            jugador1 = torneoQueue.front();
+            torneoQueue.pop();
+            rondasGanadas = 0;
+        }
+        jugador2 = torneoQueue.front();
+        torneoQueue.pop();
+
+        K--;
+    }
+
+    return resultado;
 }
 
 int main() {
-    vector<int> habilidades = {1,2,3}
-    vector<int> caso1 = Torneo(habilidades, 2, 2);
+    vector<int> habilidades1 = {1, 2, 3};
+    vector<int> habilidades2 = {49, 24, 26, 12, 5, 33, 25, 30, 35, 41, 46, 23, 21, 3, 38, 43, 11, 19, 34, 29, 20, 32, 39, 7, 50};
+    vector<int> habilidades3 = {30, 12};
+
+    vector<int> resultado1 = simularTorneo(habilidades1, 2, 2);
+    
+    vector<int> resultado2 = simularTorneo(habilidades2, 10, 399);
+    
+    vector<int> resultado3 = simularTorneo(habilidades1, 2, 4);
+    
+    vector<int> resultado4 = simularTorneo(habilidades3, 34, 80);
+
+    cout << "Caso 1: " << resultado1[0] << " " << resultado1[1] << endl;
+    cout << "Caso 2: " << resultado2[0] << " " << resultado2[1] << endl;
+    cout << "Caso 3: " << resultado3[0] << " " << resultado3[1] << endl;
+    cout << "Caso 4: " << resultado4[0] << " " << resultado4[1] << endl;
+    return 0;
 }
+
